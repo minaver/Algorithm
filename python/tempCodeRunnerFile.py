af926d2439a76bd8aebcdb9cf598759af71efc55
minaver <psmjoshua@gmail.com>
2021-12-25 23:05:52 +0900

    Modify temp files

--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,7 +1,3 @@
-T = int(input())
-# for t in range(T):
-#   n = int(input())
-#   for c in range(int(n/2),n):
-#     if sosu[c] == 1 and n - sosu[c] == 1:
-#       print(c,n-c)
-#       break
+
+N = 2
+print(str(pattern_top(N))+str(pattern_middle(N))+str(pattern_bottom(N)))
